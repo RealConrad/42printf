19286767e9d38e316bc01c4134992802143456d7
cwenz <wenzconrad@gmail.com>
2023-04-27 16:10:32 +0200

    added write change

--- ft_number_specifiers.c
+++ ft_number_specifiers.c
@@ -6,7 +6,7 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 09:45:12 by cwenz             #+#    #+#             */
-/*   Updated: 2023/04/17 16:28:33 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/04/27 16:00:21 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,38 @@
 */
 int	ft_putnum(long num)
 {
-	int	i;
+	int	length;
 
-	i = 0;
+	length = 0;
 	if (num < 0)
 	{
 		num *= -1;
-		i += ft_putchar('-');
+		length = ft_putchar('-');
+		if (length < 0)
+			return (-1);
 	}
-	if (num < 10)
-		i += ft_putchar(num % 10 + 48);
-	else if (num >= 10)
+	length += ft_print_digits(num);
+	return (length);
+}
+
+int	ft_print_digits(long num)
+{
+	int	write_check;
+	int	length;
+
+	length = 0;
+	write_check = 0;
+	if (num >= 10)
 	{
-		i += ft_putnum(num / 10);
-		i += ft_putnum(num % 10);
+		length = ft_print_digits(num / 10);
+		if (length < 0)
+			return (-1);
 	}
-	return (i);
+	write_check = ft_putchar(num % 10 + 48);
+	if (write_check < 0)
+		return (-1);
+	length += write_check;
+	return (length);
 }
 
 /**
@@ -43,7 +59,7 @@ int	ft_putnum(long num)
 ** @param c Char representing the case for hexadecimal digits (x or X)
 ** @return The number of characters printed
 */
-int	ft_puthex(long num, char c)
+int	ft_puthex(size_t num, char c)
 {
 	int		i;
 	int		length;
@@ -59,14 +75,15 @@ int	ft_puthex(long num, char c)
 		return (ft_putchar('0'));
 	while (num != 0)
 	{
-		hex_string[i] = hex[num % 16];
+		hex_string[i++] = hex[num % 16];
 		num /= 16;
-		i++;
 	}
 	while (i > 0)
 	{
-		i--;
-		length += ft_putchar(hex_string[i]);
+		if (ft_putchar(hex_string[--i]) < 0) // Check if write fails
+			return (-1);
+		else
+			length++;
 	}
 	return (length);
 }
@@ -78,39 +95,11 @@ int	ft_puthex(long num, char c)
 */
 int	ft_putp(void *ptr)
 {
-	int				i;
-	int				length;
-	char			hex_string[17];
-	char			*hex;
-	unsigned long	p;
+	int	i;
 
-	p = (unsigned long)ptr;
-	hex = "0123456789abcdef";
-	i = 0;
-	length = 0;
-	if (!ptr)
-		return (ft_putstr("0x0"));
-	while (p != 0)
-	{
-		hex_string[i] = hex[p % 16];
-		p /= 16;
-		i++;
-	}
-	length += ft_putstr("0x");
-	while (i > 0)
-	{
-		i--;
-		length += ft_putchar(hex_string[i]);
-	}
-	return (length);
+	i = ft_putstr("0x");
+	if (i < 0)
+		return (-1);
+	i += ft_puthex((size_t)ptr, 'x');
+	return (i);
 }
-
-// int	main(void)
-// {
-// 	int	length;
-
-// 	length = ft_putp(74574212);
-// 	printf("\nPRINTF: %d\n", 74574212);
-// 	printf("\nLENGTH: %d\n", length);
-// 	return (0);
-// }
