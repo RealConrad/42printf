9f4248879de6be19223479b94660beac6cab3434
cwenz <wenzconrad@gmail.com>
2023-04-27 16:10:47 +0200

    added write change

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 11:54:57 by cwenz             #+#    #+#             */
-/*   Updated: 2023/04/15 18:55:07 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/04/27 16:07:08 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-# include <stdio.h> // REMOVE THIS
 # include <stdlib.h>
 # include <unistd.h>
 
@@ -22,7 +21,9 @@ int	ft_putchar(char c);
 int	ft_putstr(char *str);
 int	ft_putp(void *ptr);
 int	ft_putnum(long num);
-int	ft_puthex(long num, char c);
+int	ft_print_digits(long num);
+int	ft_calc_length(va_list args, const char *format, int length);
+int	ft_puthex(size_t num, char c);
 int	ft_format_specifiers(int c, va_list args);
 int	ft_printf(const char *format, ...);
 
